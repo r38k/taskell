82bfe63dbdb421b981b89fd24bd7535b4cb4920d
r38k <lgmlgm6179@gmail.com>
2025-09-10 15:19:01 +0000

    feat: タスクストレージ機能の拡張と型定義の更新
    - タスクストレージインターフェースにResult型を使用したメソッドを追加
    - JSONLストレージの実装を強化し、タスクの追加、取得機能を実装
    - package.jsonとpackage-lock.jsonにpinoと@types/nodeを追加
    
    タスク管理機能の信頼性向上とデータストレージの拡張を行いました。

--- src/core/storage/index.ts
+++ src/core/storage/index.ts
@@ -1,18 +1,19 @@
 import type { ScheduledTask, Task, TaskSet } from "../type";
+import { Result } from "neverthrow";
 
 export interface TaskStorage {
     // タスク操作系
-    addTask(task: Task): void;
-    addScheduledTask(task: ScheduledTask): void;
-    addTaskSet(taskSet: TaskSet): void;
-    completeTask(id: string): void;
+    addTask(task: Task): Result<string, Error>;
+    addScheduledTask(task: ScheduledTask): Result<string, Error>;
+    addTaskSet(taskSet: TaskSet): Result<string, Error>;
+    completeTask(id: string): Result<string, Error>;
     
     // タスク取得系
-    getTaskById(id: string): Task;
-    getTaskSetById(id: string): TaskSet;
+    getTaskById(id: string): Promise<Result<Task, Error>>;
+    getTaskSetById(id: string): Promise<Result<TaskSet, Error>>;
 
     // 複数タスク取得系
-    getTasks(): Task[];
-    getScheduledTasks(): ScheduledTask[];
-    getTaskSets(): TaskSet[];
+    getTasks(): Promise<Result<Task[], Error>>;
+    getScheduledTasks(): Promise<Result<ScheduledTask[], Error>>;
+    getTaskSets(): Promise<Result<TaskSet[], Error>>;
 }
--- src/core/storage/jsonl.ts
+++ src/core/storage/jsonl.ts
@@ -1,6 +1,108 @@
 import type { TaskStorage } from "./index";
+import type { ScheduledTask, Task, TaskSet } from "../type";
+import { readFile, rename, writeFile } from "node:fs/promises";
+import { ok, err } from "neverthrow";
 
-export const jsonlStorage: TaskStorage = {
+// TODO: XDG対応?
+const BASE_PATH = "~/.config/taskell";
 
+export const jsonLinesStorage: TaskStorage = {
 
+    addTask(task: Task) {
+        try {
+            const filePath = `${BASE_PATH}/${task.id}.jsonl`;
+            const fileContent = `${JSON.stringify(task)}\n`;
+            writeFile(filePath, fileContent);
+            return ok(filePath);
+        } catch (error) {
+            return err(error);
+        }
+    },
+
+    addScheduledTask(task: ScheduledTask) {
+        try {
+            const filePath = `${BASE_PATH}/${task.id}.jsonl`;
+            const fileContent = `${JSON.stringify(task)}\n`;
+            writeFile(filePath, fileContent);
+            return ok(filePath);
+        } catch (error) {
+            return err(error);
+        }
+    },
+
+    addTaskSet(taskSet: TaskSet) {
+        try {
+            const filePath = `${BASE_PATH}/${taskSet.id}.jsonl`;
+            const fileContent = `${JSON.stringify(taskSet)}\n`;
+            writeFile(filePath, fileContent);
+            return ok(taskSet.id);
+        } catch (error) {
+            return err(error);
+        }
+    },
+
+    /**
+     * タスクファイルを完了ディレクトリへ移動
+     * @param id 
+     */
+    completeTask(id: string) {
+        try {
+            const filePath = `${BASE_PATH}/${id}.jsonl`;
+            const doneFilePath = `${BASE_PATH}/done/${id}.jsonl`;
+            rename(filePath, doneFilePath);
+            return ok(doneFilePath);
+        } catch (error) {
+            return err(error);
+        }
+    },
+
+    async getTaskById(id: string) {
+        try {
+            const filePath = `${BASE_PATH}/${id}.jsonl`;
+            const fileContent = await readFile(filePath, "utf-8");
+            return ok(JSON.parse(fileContent));
+        } catch (error) {
+            return err(error);
+        }
+    },
+
+    async getTaskSetById(id: string) {
+        try {
+            const filePath = `${BASE_PATH}/${id}.jsonl`;
+            const fileContent = await readFile(filePath, "utf-8");
+            return ok(JSON.parse(fileContent));
+        } catch (error) {
+            return err(error);
+        }
+    },
+
+    async getTasks() {
+        try {
+            const filePath = `${BASE_PATH}/*.jsonl`;
+            const fileContent = await readFile(filePath, "utf-8");
+            return ok(JSON.parse(fileContent));
+        } catch (error) {
+            return err(error);
+        }
+    },
+
+    async getScheduledTasks() {
+        try {
+            const filePath = `${BASE_PATH}/*.jsonl`;
+            const fileContent = await readFile(filePath, "utf-8");
+            return ok(JSON.parse(fileContent));
+        } catch (error) {
+            return err(error);
+        }
+    },
+
+    async getTaskSets() {
+        try {
+            const filePath = `${BASE_PATH}/*.jsonl`;
+            const fileContent = await readFile(filePath, "utf-8");
+            return ok(JSON.parse(fileContent));
+        } catch (error) {
+            return err(error);
+        }
+    },
 }
